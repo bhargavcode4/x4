07039aac6af9fd5773b1ecbdca703bd9beb84020
BhargavThota <114083873+bhargavcode4@users.noreply.github.com>
2026-04-23 10:59:12 +0530

    Delete xplane directory

--- xplane/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@libsql/client", "bcryptjs", "jsonwebtoken", "twilio"]
-  }
-};
-module.exports = nextConfig;
